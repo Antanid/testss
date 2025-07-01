0fa17664594f7f4140ecd9a00e15933d6e0fddb8
Antanid <dbalakleenko1@gmail.com>
2025-07-01 23:57:56 +0300

    updated

--- src/pages/exchanger-page/components/AutoComplete/AutoComplete.jsx
+++ src/pages/exchanger-page/components/AutoComplete/AutoComplete.jsx
@@ -1,41 +1,41 @@
-import React, { useState, useMemo, useEffect } from 'react'
-import { Modal, ModalContent, ModalBody } from '@heroui/modal'
-import styles from './Modal.module.scss'
+import React, { useState, useMemo, useEffect, useRef } from 'react'
+
 import { Check, MagnifyingGlass, X } from 'phosphor-react'
+
+import styles from './Modal.module.scss'
+import { DialogCommon } from '@/pages/exchanger-page/components/Dialog/Dialog.jsx'
 
 const AutocompleteSelect = ({
   options = [],
   onOptionClick,
   headerTitle = 'Select',
   placeholder = '',
-  value = null, // выбранный option { id, title }
-  children, // кастомный триггер для открытия модалки
+  value = null,
+  children, // кастомный триггер
 }) => {
   const [isOpen, setIsOpen] = useState(false)
   const [search, setSearch] = useState('')
 
-  // Фильтрация options по поиску
   const filteredOptions = useMemo(() => {
     const s = search.trim().toLowerCase()
     return options.filter(option => option.title.toLowerCase().includes(s))
   }, [search, options])
 
-  // Закрываем поиск при смене value (выборе)
   useEffect(() => {
     if (!isOpen) setSearch('')
   }, [isOpen])
 
-  // Выбор элемента
   const handleSelect = option => {
     onOptionClick(option)
     setIsOpen(false)
     setSearch('')
   }
 
-  const inputRef = React.useRef(null)
+  const inputRef = useRef(null)
 
   useEffect(() => {
-    if (isOpen) {
+    if (isOpen && inputRef.current) {
+      inputRef.current.focus()
       document.body.style.overflow = 'hidden'
     } else {
       document.body.style.overflow = ''
@@ -47,120 +47,86 @@ const AutocompleteSelect = ({
 
   return (
     <>
-      {/* Кастомный триггер открытия */}
-      <div onClick={() => setIsOpen(true)} style={{ cursor: 'pointer' }}>
-        {children}
-      </div>
-
-      <Modal
+      <DialogCommon
         isOpen={isOpen}
-        onOpenChange={setIsOpen}
-        hideCloseButton
-        shadow="none"
-        isDismissable
-        scrollBehavior="inside"
+        onChange={setIsOpen}
+        trigger={<div style={{ cursor: 'pointer' }}>{children}</div>}
+        contentClassNames={styles.modal_content}
+        disablePadding={true}
+        size="md"
       >
-        <ModalContent
-          style={{
-            position: 'fixed',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            width: '600px',
-            maxWidth: '90vw',
-            maxHeight: '80vh', // фиксируем максимальную высоту модалки
-            borderTopLeftRadius: '16px',
-            borderTopRightRadius: '16px',
-            border: 'none',
-            zIndex: 100,
-            backgroundColor: 'var(--bg-color)',
-            display: 'flex',
-            flexDirection: 'column',
-          }}
-        >
-          <ModalBody
-            className={styles.modal_content}
-            style={{
-              padding: 0, // если нужно убрать лишние отступы
-              flexGrow: 1, // чтобы занять всю доступную высоту внутри ModalContent
-              display: 'flex',
-              flexDirection: 'column',
-            }}
-          >
-            <div className={styles.autoSelect__menu}>
-              <div className={styles.autoSelect__header}>
-                <div className={styles.autoSelect__headerContent}>
-                  <p className="text" style={{ fontWeight: 700, color: 'white', fontSize: '20px' }}>
-                    {headerTitle}
-                  </p>
-                  <X
-                    size={24}
-                    style={{ cursor: 'pointer' }}
-                    color="#fff"
-                    onClick={() => setIsOpen(false)}
-                  />
-                </div>
-              </div>
+        <div className={styles.autoSelect__menu}>
+          <div className={styles.autoSelect__header}>
+            <div className={styles.autoSelect__headerContent}>
+              <p className="text" style={{ fontWeight: 700, color: 'white', fontSize: 20 }}>
+                {headerTitle}
+              </p>
+              <X
+                size={24}
+                style={{ cursor: 'pointer' }}
+                color="#fff"
+                onClick={() => setIsOpen(false)}
+              />
+            </div>
+          </div>
 
-              <div className={styles.autoSelect__searchWrapper}>
-                <div className={styles.autoSelect__searchContent}>
-                  <MagnifyingGlass size={24} />
-                  <input
-                    type="text"
-                    value={search}
-                    onChange={e => setSearch(e.target.value)}
-                    placeholder={placeholder}
-                    className={styles.autoSelect__input}
-                    onClick={e => e.stopPropagation()}
-                    ref={inputRef}
-                    autoFocus={false}
-                  />
-                </div>
-              </div>
+          <div className={styles.autoSelect__searchWrapper}>
+            <div className={styles.autoSelect__searchContent}>
+              <MagnifyingGlass size={24} />
+              <input
+                type="text"
+                value={search}
+                onChange={e => setSearch(e.target.value)}
+                placeholder={placeholder}
+                className={styles.autoSelect__input}
+                onClick={e => e.stopPropagation()}
+                ref={inputRef}
+                autoFocus={false}
+              />
+            </div>
+          </div>
 
-              <ul className={styles.autoSelect__list}>
-                {filteredOptions.length > 0 ? (
-                  filteredOptions.map(option => {
-                    const isSelected = value?.id === option.id
-                    return (
-                      <li
-                        key={option.id}
-                        className={styles.autoSelect__item}
-                        onClick={() => handleSelect(option)}
-                        tabIndex={0}
-                        onKeyDown={e => {
-                          if (e.key === 'Enter' || e.key === ' ') {
-                            handleSelect(option)
-                          }
+          <ul className={styles.autoSelect__list}>
+            {filteredOptions.length > 0 ? (
+              filteredOptions.map(option => {
+                const isSelected = value?.id === option.id
+                return (
+                  <li
+                    key={option.id}
+                    className={styles.autoSelect__item}
+                    onClick={() => handleSelect(option)}
+                    tabIndex={0}
+                    onKeyDown={e => {
+                      if (e.key === 'Enter' || e.key === ' ') {
+                        handleSelect(option)
+                      }
+                    }}
+                  >
+                    {option.title}
+                    {isSelected && (
+                      <div
+                        style={{
+                          display: 'flex',
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                          borderRadius: '100%',
+                          padding: '4px',
+                          background: '#8947FF',
+                          marginLeft: 'auto',
                         }}
                       >
-                        {option.title}
-                        {isSelected && (
-                          <div
-                            style={{
-                              display: 'flex',
-                              justifyContent: 'center',
-                              alignItems: 'center',
-                              borderRadius: '100%',
-                              padding: '4px',
-                              background: '#8947FF',
-                              marginLeft: 'auto',
-                            }}
-                          >
-                            <Check size={16} color="#fff" />
-                          </div>
-                        )}
-                      </li>
-                    )
-                  })
-                ) : (
-                  <li className={styles.autoSelect__noResults}>Совпадений не найдено</li>
-                )}
-              </ul>
-            </div>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
+                        <Check size={16} color="#fff" />
+                      </div>
+                    )}
+                  </li>
+                )
+              })
+            ) : (
+              <li className={styles.autoSelect__noResults}>Совпадений не найдено</li>
+            )}
+          </ul>
+        </div>
+      </DialogCommon>
     </>
   )
 }
---
+++ src/pages/exchanger-page/components/Dialog/Dialog.jsx
@@ -0,0 +1,29 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import styles from './Dialog.module.scss'
+
+export const DialogCommon = ({
+  children,
+  isOpen,
+  onChange,
+  trigger,
+  contentClassNames,
+  size = 'md',
+}) => {
+  return (
+    <Dialog.Root open={isOpen} onOpenChange={onChange}>
+      {trigger && <Dialog.Trigger asChild>{trigger}</Dialog.Trigger>}
+      <Dialog.Portal>
+        <Dialog.Overlay className={styles.overlayDimmed} />
+
+        <Dialog.Content
+          onOpenAutoFocus={event => event.preventDefault()}
+          className={[styles.content, styles[size], contentClassNames || '']
+            .filter(Boolean)
+            .join(' ')}
+        >
+          {children}
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
+}
